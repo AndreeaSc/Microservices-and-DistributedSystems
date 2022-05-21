fe10237307315e77969a2c38951c6184ad68e59e
Andreea Schiau <schiau.andreea@gmail.com>
2022-05-21 23:59:43 +0300

    Add Notification service

---
+++ notification/src/main/java/com/microservices/notification/NotificationRepository.java
@@ -0,0 +1,5 @@
+package com.microservices.notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Integer> {
+}
